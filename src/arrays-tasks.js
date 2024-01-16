4353e220bbbeba007743ec9e068143bb1a70b764
Ksyusha <74423375+ksen4ik@users.noreply.github.com>
2024-01-16 23:20:12 +0300

    feat: implement generateOdds task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -385,8 +385,12 @@ function createChunks(arr, chunkSize) {
  *    generateOdds(2) => [ 1, 3 ]
  *    generateOdds(5) => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  if (len === 0) return [];
+
+  const arr = Array.from({ length: len }, (_, i) => i * 2 + 1);
+
+  return arr;
 }
 
 /**
